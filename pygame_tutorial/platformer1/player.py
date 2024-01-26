3e66429152867ec60b0588eae31cc1d4bb99bfec
msa-sadeghi <sadeghi.msai@gmail.com>
2024-01-26 12:29:15 +0330

    دلیل باگ
    نامناسب ابعاد عکس
    عکس عوض شد

--- pygame_tutorial/platformer1/player.py
+++ pygame_tutorial/platformer1/player.py
@@ -12,20 +12,22 @@ class Player(Sprite):
         self.idle_right = []
         self.idle_left = []
         # این حلقه برای اضافه کردن تصاویر بازیکن در حال حرکت به سمت راست و چپ نوشته شده است
-        for i in range(1, 9):
-            temp_img = pygame.image.load(f"assets/boy/Run ({i}).png")
-            image = pygame.transform.scale(temp_img, (temp_img.get_width() * 0.2, temp_img.get_height() * 0.2))
+        for i in range(1, 5):
+            # temp_img = pygame.image.load(f"assets/boy/Run ({i}).png")
+            # image = pygame.transform.scale(temp_img, (temp_img.get_width() * 0.2, temp_img.get_height() * 0.2))
+            image = pygame.image.load(f"assets/boy/guy{i}.png")
+            image = pygame.transform.scale(image, (image.get_width() * 0.5, image.get_height() * 0.5))
             self.right_images.append(image)
             image = pygame.transform.flip(image, True, False)
             self.left_images.append(image)
 
         # این حلقه برای لود کردن و اضافه کردن عکس های بازیکن که بدون حرکت است می باشد
-        for i in range(1, 10):
-            img = pygame.image.load(f"assets/boy/Idle ({i}).png")
-            img = pygame.transform.scale(img, (img.get_width() * 0.2, img.get_height() * 0.2))
-            self.idle_right.append(img)
-            img = pygame.transform.flip(img, True, False)
-            self.idle_left.append(img)
+        # for i in range(1, 10):
+        #     img = pygame.image.load(f"assets/boy/Idle ({i}).png")
+        #     img = pygame.transform.scale(img, (img.get_width() * 0.2, img.get_height() * 0.2))
+        #     self.idle_right.append(img)
+        #     img = pygame.transform.flip(img, True, False)
+        #     self.idle_left.append(img)
 
         self.frame_index = 0
         self.counter = 0
@@ -38,12 +40,12 @@ class Player(Sprite):
         self.vel_y = 0
 
     def update(self, tiles_map):
-        # pygame.draw.rect(SCREEN, (190, 30, 230), self.rect, 5)
-        if self.direction == 1:
-            rect = pygame.Rect(self.rect.x + 50, self.rect.y + 15, self.image.get_width()- 80, self.image.get_height()-24)
-        if self.direction == -1:
-            rect = pygame.Rect(self.rect.x + 30, self.rect.y + 15, self.image.get_width()- 80, self.image.get_height()-24)
-        pygame.draw.rect(SCREEN, (30, 190, 230), rect, 5)
+        pygame.draw.rect(SCREEN, (190, 30, 230), self.rect, 5)
+        # if self.direction == 1:
+        #     rect = pygame.Rect(self.rect.x + 50, self.rect.y + 15, self.image.get_width()- 80, self.image.get_height()-24)
+        # if self.direction == -1:
+        #     rect = pygame.Rect(self.rect.x + 30, self.rect.y + 15, self.image.get_width()- 80, self.image.get_height()-24)
+        # pygame.draw.rect(SCREEN, (30, 190, 230), rect, 5)
 
         dx = 0
         dy = 0
@@ -66,16 +68,16 @@ class Player(Sprite):
         dy += self.vel_y
 
         for tile in tiles_map:
-            if tile[1].colliderect(rect.x + dx, rect.y, self.image.get_width()- 80, self.image.get_height()-24):
+            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.image.get_width(), self.image.get_height()):
                 dx = 0
-            if tile[1].colliderect(rect.x, rect.y + dy, self.image.get_width()- 80, self.image.get_height()-24):
+            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.image.get_width(), self.image.get_height()):
                 
                 if self.vel_y < 0:
                     self.vel_y = 0
-                    dy = tile[1].bottom - rect.top
-                else:
+                    dy = tile[1].bottom - self.rect.top
+                elif self.vel_y > 0:
                     self.vel_y = 0
-                    dy = tile[1].top - rect.bottom
+                    dy = tile[1].top - self.rect.bottom
 
 
 
@@ -98,10 +100,10 @@ class Player(Sprite):
         if self.direction == 1:
             if self.moving:
                 self.image = self.right_images[self.frame_index]
-            else:
-                self.image = self.idle_right[self.frame_index]
+            # else:
+            #     self.image = self.idle_right[self.frame_index]
         if self.direction == -1:
             if self.moving:
                 self.image = self.left_images[self.frame_index]
-            else:
-                self.image = self.idle_left[self.frame_index]
+            # else:
+            #     self.image = self.idle_left[self.frame_index]
