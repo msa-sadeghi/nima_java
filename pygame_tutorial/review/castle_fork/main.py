5f42485033fa41af7ede9c4bd57e1b4588eb8a65
msa-sadeghi <sadeghi.msai@gmail.com>
2024-08-09 10:53:49 +0330

    a

--- pygame_tutorial/review/castle_fork/main.py
+++ pygame_tutorial/review/castle_fork/main.py
@@ -1,22 +1,31 @@
 import pygame
-# from player import Player
+from player import Player
+import random
 screen = pygame.display.set_mode((800, 600))
 
 clock = pygame.time.Clock()
 FPS = 60
-
-# p = Player(100, 200)
+player_group = pygame.sprite.Group()
+player_types = ("goblin" ,"red_goblin" ,"purple_goblin" , "knight")
+x = 0
+y = 200
+for i in range(4):
+    # p = Player(player_types[i % len(player_types)], x, y)
+    p = Player(player_types[random.randrange(len(player_types))], x, y)
+    player_group.add(p)
+    x += 200
+    
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0,0,0))
-    # p.draw(screen)   
-    # p.animation()    
+    for player in player_group:
+        if p.moving_or_not:
+            player.animation()    
+        player.draw(screen)
+        player.move()
     pygame.display.update()
     clock.tick(FPS)
     
-    
-# -> player1 animation
-# d  player2 animation
--- pygame_tutorial/review/castle_fork/player.py
+++ pygame_tutorial/review/castle_fork/player.py
@@ -1,20 +1,21 @@
 import pygame
 from pygame.sprite import Sprite
 class Player(Sprite):
-    def __init__(self, x,y):
+    def __init__(self,type, x,y):
         super().__init__()
         self.animation_types = ("walk", "attack", "death")
         self.all_images = []
         for anim in self.animation_types:
             temp = []
             for i in range(20):
-                img = pygame.image.load(f"assets/enemies/goblin/{anim}/{i}.png")
+                img = pygame.image.load(f"assets/enemies/{type}/{anim}/{i}.png")
                 img = pygame.transform.scale(img, (200, 150))
                 temp.append(img)
             self.all_images.append(temp)
         self.image = self.all_images[0][0]
         self.rect = self.image.get_rect(topleft = (x,y))
         self.image_number = 0
+        self.moving_or_not = False
     def draw(self, screen):
         self.image = self.all_images[0][self.image_number]
         screen.blit(self.image, self.rect)
@@ -22,6 +23,18 @@ class Player(Sprite):
         self.image_number += 1
         if self.image_number >= len(self.all_images[0]):
             self.image_number = 0
+            
+    def move(self):
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.moving_or_not = True
+        if keys[pygame.K_RIGHT]:
+            self.moving_or_not = True
+        if not  keys[pygame.K_LEFT]   and   not  keys[pygame.K_RIGHT]:
+            self.moving_or_not = False
+            
+            
         
                 
             
