8b937e743ad9fe386e9600ea34eae663f3abecd6
msa-sadeghi <sadeghi.msai@gmail.com>
2024-11-23 17:00:40 +0330

    Create 9.py

---
+++ pygame_tutorial/review/9.py
@@ -0,0 +1,37 @@
+# weather = input("is raining? ('yes or no')")
+# if weather == "no":
+#     print("we can go to park")
+# else:
+#     print("we can't go to park")
+    
+# running = True
+# if running == True:
+#     print("ok")
+
+# x = {
+#     1 : "a",
+#     2 : "b",
+#     3 : "c"
+# }
+
+# print(x[1])
+# print(x[2])
+# print(x[3])
+# x[4] = "d"
+# print(x)
+# del x[2]
+# print(x)
+
+# student = {}
+# name = input("enter your name: ")
+# family = input("enter your family: ")
+# age = input("enter your age: ")
+# favorite_sport = input("enter your favorite sport: ")
+
+# student["name"] = name
+# student["surname"] = family
+# student["age"] = age
+# student["sport"] = favorite_sport
+# print(student)
+
+# favorite foods  4
