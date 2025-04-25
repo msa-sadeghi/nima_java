061bd7b92550e83ab5e775bd94eb19836b4adc23
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-04-25 10:57:56 +0330

    shoot animation

--- pygame_tutorial/robot/main.py
+++ pygame_tutorial/robot/main.py
@@ -8,7 +8,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 
 
-my_player = Player(100, 100)
+my_player = Player(100, 300)
 clock = pygame.time.Clock()
 FPS = 60
 running = True
@@ -17,6 +17,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0, 0, 0))
+    if my_player.shoot:
+        my_player.change_animation('Shoot')
+    elif my_player.in_air:
+        my_player.change_animation('Jump')
+    elif my_player.slide:
+        my_player.change_animation('Slide')
+    elif my_player.idle:
+        my_player.change_animation('Idle')
+    else:
+        my_player.change_animation('Run')
     my_player.draw(screen)
     my_player.move()
     pygame.display.update()
--- pygame_tutorial/robot/player.py
+++ pygame_tutorial/robot/player.py
@@ -21,18 +21,56 @@ class Player(Sprite):
         self.image = self.all_images[self.action][self.frame_index]
         self.rect = self.image.get_rect(topleft=(x,y))
         self.last_update_time = pygame.time.get_ticks()
+        self.flip = False
+        self.idle = True
+        self.direction = 1
+        self.slide = False
+        self.in_air = False
+        self.gravity = 0.5
+        self.shoot = False
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        screen.blit(
+            pygame.transform.flip(self.image, self.flip, False)
+            , self.rect)
         self.animation()
     def move(self):
         dx = 0
         dy = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            self.flip  = True
+            self.direction = -1
+            self.idle = False
             dx += -5
         elif keys[pygame.K_RIGHT]:
+            self.flip = False
+            self.direction = 1
+            self.idle = False
             dx += 5
+        if not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            self.idle = True
+
+        if keys[pygame.K_DOWN]:
+            self.slide = True
+            dx += self.direction * 5
+
+        else:
+            self.slide = False
+        if keys[pygame.K_UP] and not self.in_air:
+            self.in_air = True
+            self.gravity = -10
+        if keys[pygame.K_SPACE]:
+            self.shoot = True
+        else:
+            self.shoot = False
+    
+        dy += self.gravity
+        self.gravity += 0.5
+        if self.rect.bottom + dy >= 600:
+            self.in_air = False
+            dy = 600 - self.rect.bottom
+            self.gravity = 0
         self.rect.x += dx
         self.rect.y += dy
 
@@ -43,4 +81,10 @@ class Player(Sprite):
             self.last_update_time = pygame.time.get_ticks()
             if self.frame_index >= len(self.all_images[self.action]):
                 self.frame_index = 0
+
+    def change_animation(self,  new):
+        if new != self.action:
+            self.action = new
+            self.frame_index = 0
+            self.last_update_time = pygame.time.get_ticks()
       
