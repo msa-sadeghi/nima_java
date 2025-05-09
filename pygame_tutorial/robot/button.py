26ef17ab431bfc00614a3c3ef78b306c7d29d235
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-09 11:00:03 +0330

    buttons

---
+++ pygame_tutorial/robot/button.py
@@ -0,0 +1,17 @@
+import pygame
+class Button:
+    def __init__(self, image, x,y, type):
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x,y))
+        self.type = type
+
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect)
+        clicked = True
+        mouse_position = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_position):
+            if pygame.mouse.get_pressed()[0]:
+                clicked = False
+
+        return clicked
--- pygame_tutorial/robot/level_editor.py
+++ pygame_tutorial/robot/level_editor.py
@@ -1,5 +1,7 @@
 import pygame
 pygame.init()
+import os
+from button import Button
 screen_width = 1000
 SIDE_MARGIN = 400
 screen_height = 640
@@ -7,6 +9,45 @@ LOWER_MARGIN = 100
 screen = pygame.display.set_mode((screen_width + SIDE_MARGIN, screen_height + LOWER_MARGIN))
 pygame.display.set_caption("Level Editor")
 clock = pygame.time.Clock()
+TILE_SIZE = 50
+ROWS = 20
+COLS = 150
+def draw_lines():
+    for i in range(ROWS + 1):
+        pygame.draw.line(screen, "brown", (0, i * TILE_SIZE), (screen_width, i *TILE_SIZE))
+    for i in range(COLS + 1):
+        pygame.draw.line(screen, "brown", (i * TILE_SIZE, 0), (i *TILE_SIZE, screen_height))
+
+
+objects_images = []
+for img in os.listdir("./game_world/Objects"):
+    image = pygame.image.load(f"./game_world/Objects/{img}")
+    image = pygame.transform.scale(image, (TILE_SIZE, TILE_SIZE))
+    objects_images.append(image)
+
+tiles_images = []
+for img in os.listdir("./game_world/Tiles"):
+    image = pygame.image.load(f"./game_world/Tiles/{img}")
+    image = pygame.transform.scale(image, (TILE_SIZE, TILE_SIZE))
+    tiles_images.append(image)
+
+r = 0
+c = 0
+buttons_list = []
+for i in range(len(objects_images)):
+    buttons_list.append(Button(objects_images[i], screen_width + 40 + c * 70, 70 + r * 70, 'object'))
+    c += 1
+    if c == 5:
+        r += 1
+        c = 0
+for i in range(len(tiles_images)):
+    buttons_list.append(Button(tiles_images[i], screen_width + 40 + c * 70, 70 + r * 70, 'object'))
+    c += 1
+    if c == 5:
+        r += 1
+        c = 0
+
+
 FPS = 60
 running = True  
 while running:
@@ -15,8 +56,11 @@ while running:
             running = False
 
     screen.fill("white")  # Fill the screen with black
+    draw_lines()
     pygame.draw.rect(screen, "lightblue", (screen_width, 0, SIDE_MARGIN, screen_height + LOWER_MARGIN))  # Draw the black rectangle
     pygame.draw.rect(screen, "lightblue", (0, screen_height, screen_width, LOWER_MARGIN))  # Draw the black rectangle
+    for btn in buttons_list:
+        btn.update(screen)
     pygame.display.flip()  # Update the display
     clock.tick(FPS)  # Control the frame rate
 pygame.quit()
