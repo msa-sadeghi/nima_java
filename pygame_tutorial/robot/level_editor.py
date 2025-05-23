f81ceecffdb7fbf3f7e2f41bf7464b660d0fa68b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-05-23 12:35:18 +0330

    add click to buttons

--- pygame_tutorial/robot/level_editor.py
+++ pygame_tutorial/robot/level_editor.py
@@ -49,6 +49,20 @@ for i in range(len(tiles_images)):
 
 selected_button_index = 0
 FPS = 60
+
+world_map = []
+for i in range(ROWS):
+    row = [-1] * COLS
+    world_map.append(row)
+
+def draw_world():
+    for i in range(ROWS):
+        for j in range(COLS):
+            if world_map[i][j] != -1:
+                screen.blit(buttons_list[world_map[i][j]].image, (j * TILE_SIZE, i * TILE_SIZE))
+
+
+
 running = True  
 while running:
     for event in pygame.event.get():
@@ -59,9 +73,18 @@ while running:
     draw_lines()
     pygame.draw.rect(screen, "lightblue", (screen_width, 0, SIDE_MARGIN, screen_height + LOWER_MARGIN))  # Draw the black rectangle
     pygame.draw.rect(screen, "lightblue", (0, screen_height, screen_width, LOWER_MARGIN))  # Draw the black rectangle
+    draw_world()
     for i,btn in enumerate(buttons_list):
        if btn.update(screen) == True:
             selected_button_index = i
+    
+    mouse_pos = pygame.mouse.get_pos()
+    if  mouse_pos[0] < screen_width and mouse_pos[1] < screen_height:
+        r = mouse_pos[1] //  TILE_SIZE
+        c = mouse_pos[0] //  TILE_SIZE
+        if pygame.mouse.get_pressed()[0]:
+            world_map[r][c] = selected_button_index
+    
     pygame.draw.rect(screen, "orangered", buttons_list[selected_button_index].rect, 3)
     pygame.display.flip()  # Update the display
     clock.tick(FPS)  # Control the frame rate
