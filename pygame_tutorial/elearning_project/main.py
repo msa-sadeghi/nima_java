60e892da84e1fc04a8dd962722c2f49c3753c512
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-07-04 12:59:53 +0330

    codes

---
+++ pygame_tutorial/elearning_project/main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("برنامه آموزشی هوشمند")
+    root.geometry("500x400")
+
+    root.mainloop()
+
+
+# if __name__ == "__main__":
+main()
