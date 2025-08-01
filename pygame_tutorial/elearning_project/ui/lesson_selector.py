b5659af7a50c43c5f5bb41b69aa5f41690a83ced
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-01 13:03:02 +0330

    codes

--- pygame_tutorial/elearning_project/ui/lesson_selector.py
+++ pygame_tutorial/elearning_project/ui/lesson_selector.py
@@ -7,9 +7,9 @@ def show_lessons(frame, subject, on_select_lesson, on_back):
     tk.Label(frame, text=f"فصل های درس {subject}",
              bg="lightblue", fg="purple", font=("ZahraRoosta", 14)).pack(pady=10)
     
-    if on_back:
-        tk.Button(frame, text="بازکشت",font=("ZahraRoosta", 12), command=on_back).pack(pady=10)
 
     for chapter in lessons.get(subject):
 
         tk.Button(frame, text=chapter, font=("ZahraRoosta", 12)).pack(pady=10)
+    if on_back:
+        tk.Button(frame, text="بازکشت",font=("ZahraRoosta", 12), command=on_back).pack(pady=10)
