5796cd0753a3ce22d6099d6cab49af686e71e19a
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-15 11:02:53 +0330

    codes

--- pygame_tutorial/elearning_project/ui/lesson_selector.py
+++ pygame_tutorial/elearning_project/ui/lesson_selector.py
@@ -1,15 +1,28 @@
 import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 from data.lesson_data import lessons
+
+def show_chapter_content(frame, subject, chapter, on_back):
+    for widget in  frame.winfo_children():
+        widget.destroy()
+    content = lessons[subject][chapter].get("content", "محتوایی برای این فصل وجود ندارد")
+
+    ttk.Label(frame, text=f"{chapter}", style="TLabel").pack(pady=10)
+    ttk.Label(frame, text=f"{content}", style="TLabel", wraplength=400, justify='right').pack(pady=10)
+   
+    ttk.Button(frame, text="بازگشت",bootstyle=(INFO, OUTLINE),
+                command=lambda : show_lessons(frame, subject, None, on_back)).pack(pady=10)
+
+
 def show_lessons(frame, subject, on_select_lesson, on_back):
     for widget in  frame.winfo_children():
         widget.destroy()
 
-    tk.Label(frame, text=f"فصل های درس {subject}",
-             bg="lightblue", fg="purple", font=("B Nazanin", 14)).pack(pady=10)
-    
-
+    ttk.Label(frame, text=f"فصل های درس {subject}",style="TLabel").pack(pady=10)
     for chapter in lessons.get(subject):
-
-        tk.Button(frame, text=chapter, font=("B Nazanin", 12)).pack(pady=10)
+        print("chapter is", chapter)
+        ttk.Button(frame, text=chapter, bootstyle=(INFO, OUTLINE),
+                   command=lambda c=chapter : show_chapter_content(frame, subject, c, on_back)).pack(pady=10)
     if on_back:
-        tk.Button(frame, text="بازکشت",font=("B Nazanin", 12), command=on_back).pack(pady=10)
+        ttk.Button(frame, text="بازگشت",bootstyle=(INFO, OUTLINE), command=on_back).pack(pady=10)
