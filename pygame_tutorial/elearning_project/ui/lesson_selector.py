51f4f4a058a48c665de6cee6dbf7728f5dd22e1f
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-22 10:58:14 +0330

    code

--- pygame_tutorial/elearning_project/ui/lesson_selector.py
+++ pygame_tutorial/elearning_project/ui/lesson_selector.py
@@ -7,9 +7,20 @@ def show_chapter_content(frame, subject, chapter, on_back):
     for widget in  frame.winfo_children():
         widget.destroy()
     content = lessons[subject][chapter].get("content", "محتوایی برای این فصل وجود ندارد")
-
+    icon_path = lessons[subject][chapter].get("icon")
     ttk.Label(frame, text=f"{chapter}", style="TLabel").pack(pady=10)
-    ttk.Label(frame, text=f"{content}", style="TLabel", wraplength=400, justify='right').pack(pady=10)
+    if icon_path:
+        try:
+            icon = tk.PhotoImage(file=icon_path)
+            icon = icon.subsample(5,5)
+        except Exception as e:
+            print(e)
+    if icon:
+
+        label = ttk.Label(frame, text=f"{content}", style="TLabel", 
+                          wraplength=400, justify='right', image=icon,compound=RIGHT)
+        label.image = icon
+        label.pack(pady=10)
    
     ttk.Button(frame, text="بازگشت",bootstyle=(INFO, OUTLINE),
                 command=lambda : show_lessons(frame, subject, None, on_back)).pack(pady=10)
@@ -21,7 +32,6 @@ def show_lessons(frame, subject, on_select_lesson, on_back):
 
     ttk.Label(frame, text=f"فصل های درس {subject}",style="TLabel").pack(pady=10)
     for chapter in lessons.get(subject):
-        print("chapter is", chapter)
         ttk.Button(frame, text=chapter, bootstyle=(INFO, OUTLINE),
                    command=lambda c=chapter : show_chapter_content(frame, subject, c, on_back)).pack(pady=10)
     if on_back:
